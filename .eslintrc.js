316d6f5892fbb3203ad39ed8af401a21396b3ee3
Brandon Patterson <brandnpatterson@outlook.com>
2020-04-02 20:13:52 -0500

    add prettier

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+  },
+  extends: ["eslint:recommended", "eslint-config-prettier"],
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  globals: {},
+  rules: {},
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { home, auth, profile } = require('../controllers');
+const { home, auth, profile } = require("../controllers");
 
-router.get('/', home);
-router.get('/auth', auth);
-router.get('/profile/:username', profile);
+router.get("/", home);
+router.get("/auth", auth);
+router.get("/profile/:username", profile);
 
 module.exports = router;
