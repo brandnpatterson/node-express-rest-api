e02744332a06122b8260cdc1320c8c13e819ffe0
Brandon Patterson <brandnpatterson@outlook.com>
2020-04-06 00:27:37 -0500

    use sequelize-cli

--- index.js
+++ index.js
@@ -2,15 +2,10 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('./config/database');
 const router = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-db.authenticate()
-  .then(() => console.log('MySQL connected'))
-  .catch(err => console.log(`Error: ${err}`));
 
 app.set('view engine', 'pug');
 app.use(express.static('client'));
--- models/Post.js
+++ models/Post.js
@@ -1,13 +1,15 @@
-const Sequelize = require('sequelize');
-const db = require('../config/database');
-
-const Post = db.define('post', {
-  title: {
-    type: Sequelize.STRING
-  },
-  content: {
-    type: Sequelize.STRING
-  }
-});
-
-module.exports = Post;
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Post = sequelize.define(
+    'Post',
+    {
+      title: DataTypes.STRING,
+      content: DataTypes.STRING
+    },
+    {}
+  );
+  Post.associate = function (models) {
+    // associations can be defined here
+  };
+  return Post;
+};
