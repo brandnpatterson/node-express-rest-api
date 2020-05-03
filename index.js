540a048f7be20f7c615ee7ecbcf87cd280bee95f
Brandon Patterson <brandnpatterson@outlook.com>
2020-05-02 19:42:30 -0500

    remove client

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 const express = require('express');
-const path = require('path');
 const usersRoutes = require('./routes/users');
 
 const app = express();
@@ -10,13 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use('/api/users', usersRoutes);
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 
 app.listen(port, () => {
   console.log(`Node is listening at http://localhost:${port}`);
